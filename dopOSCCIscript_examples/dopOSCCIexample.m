5901c997260deba7e77c1d57ae159cbb12f73cdd
nicalbee <nicholas.badcock@gmail.com>
2015-09-11 16:28:25 +1000

    Playing with summary calculations
    Still unsure about labelling

--- dopOSCCIscript_examples/dopOSCCIexample.m
+++ dopOSCCIscript_examples/dopOSCCIexample.m
@@ -46,7 +46,7 @@ dop.save.summary = {'overall','epoch'}; % vs 'epoch'
 dop.save.channels = {'Difference'};
 dop.save.periods = {'poi'};
 dop.save.epochs = {'screen','odd','even'};
-dop.save.variables = {'peak_n','peak_mean','peak_sd','peak_latency'};
+dop.save.variables = {'peak_n','peak_mean','peak_sd_of_mean','peak_latency'};
 
 dop.save.save_file = []; % this will be auto completed based upon the dop.def.task_name variable
 % dop.save.save_dir = 'C:\Users\mq20111600\Documents\nData\dopStep';
